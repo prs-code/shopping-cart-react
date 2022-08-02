380be2a0e7db0e65d49eebe2e9087eef10268744
mostafa mousavi <1374.seyedmostafa@gmail.com>
2022-08-02 15:36:01 +0430

    fixed a bug of details component

--- src/components/ProductsDetails.js
+++ src/components/ProductsDetails.js
@@ -1,18 +1,27 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
 
 //Context
-import {productsContext} from '../context/ProductContext'; 
+// import {productsContext} from '../context/ProductContext'; 
 //styles
-import styles from './shared/details.module.css'
+import styles from './shared/details.module.css';
+//axios
+import axios from 'axios';
+
+
 const ProductsDetails = (props) => {
     const {id} = useParams();
-    const data = useContext(productsContext);
-    const product = data[id - 1];
-    const {image, title, description, price, category} = product;
-  
+    const [products, setProducts] =useState([]);
+    const {image, title, description, price, category} = products;
+
+    useEffect(() => {
+      const BASE_URL =" https://fakestoreapi.com/products/";
+      axios.get(`${BASE_URL}${id}`)
+        .then(product => setProducts(product.data))
+      }, [id]);
+      
     return (
-        <div className={styles.container}>
+          <div className={styles.container}>
           <img className={styles.image} src={image} alt="product" />
           <div className={styles.textContainer}>
             <h3>{title}</h3>
